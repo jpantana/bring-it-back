cdc2a2a200714e69bb4dcfdfdd0e98014ef62bf2
JPantana <joshpantana@me.com>
2019-07-18 18:32:59 -0500

    home items are printing

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
-
+// JSs
 import usersData from '../../helpers/data/usersData';
-
+import itemsData from '../../helpers/data/itemsData';
+import ItemCard from '../ItemCard/ItemCard';
+import itemCategoriesData from '../../helpers/data/itemCategoriesData';
+// STYLEs
 import './Home.scss';
 
+// const defaultItemState = {
+//   name: '',
+//   category: 'Category',
+//   condition: '',
+//   categoryId: '',
+//   ownerId: '',
+//   description: '',
+//   imageUrl: '',
+//   isAvailable: true,
+//   priceperday: '',
+//   priceperhour: '',
+// };
 class Home extends React.Component {
+  state = {
+    items: [],
+    categories: [],
+  }
+
   componentDidMount() {
     const { uid } = firebase.auth().currentUser;
     this.getUser(uid);
-    console.error(this.props.username, 'username home');
+    this.getItems();
   }
 
+  getItems = () => {
+    itemsData.getAllItems().then((res) => {
+      this.setState({ items: res });
+    }).catch(err => console.error('no items to display', err));
+  };
+
+  // Callback function displays users name in top nav
   getUser = (uid) => {
     usersData.getUsers(uid)
       .then((resp) => {
@@ -22,9 +49,35 @@ class Home extends React.Component {
       }).catch(err => console.error(err, 'no such user exists'));
   };
 
+  // CATEGORIES
+  showCategories = () => {
+    itemCategoriesData.getCategories()
+      .then((resp) => {
+        this.setState({ categories: resp });
+      })
+      .catch(err => console.error('no categories to speak of', err));
+  };
+
+
   render() {
+    const { items } = this.state;
+    const makeItemCards = items.map(item => (
+      <div>
+        <ItemCard
+          key={item.id}
+          item={ item }
+        />
+      </div>
+    ));
+
     return (
-      <h1>Home</h1>
+      <div className="Home justify-content-between">
+        <div className="row flex-nowrap">
+          { (items.length > 0 ? makeItemCards : '') }
+        </div>
+        <div className="">
+        </div>
+      </div>
     );
   }
 }
---
+++ src/Components/ItemCard/ItemCard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+// JSs
+import itemShape from '../../helpers/propz/itemShape';
+// STYLEs
+import './ItemCard.scss';
+
+class ItemCard extends React.Component {
+  static propTypes = {
+    item: itemShape.itemShape,
+  }
+
+  render() {
+    const { item } = this.props;
+
+    return (
+      <div className="ItemCard card">
+        <div className="card-body" onClick={this.singleItem}>
+          <img src={item.imageUrl} alt={item.name} className="card-img-top" />
+          <h5 className="card-title">{item.name}</h5>
+        </div>
+        <div className="allCardIcons">
+          {/* <span>{item.isAvailable === true ? <img src={checkIcon} alt="checkbox icon svg" /> : 'Rented' }</span> */}
+          <span className="editDeleteSpan">
+            {/* <img onClick={deleteItemEvent} src={deleteIcon} id={item.id} alt="delete icon"/>
+            <img onClick={editItemEvent} src={editIcon} id={`${item.id}.edit`} alt="edit icon"/> */}
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ItemCard;
